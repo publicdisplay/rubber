356059c5fba49f7be400afc0967e9f8f68c86997
Chris Shoemaker <chris.shoemaker@cox.net>
2008-05-17 11:14:24 -0400

    Transform configuration files in a predictable order.
    It's probably bad form to depend on the order of the transformation
    of files, but sometimes it's necessary.  This change makes rubber
    transform files in lexicographic order.

--- lib/rubber/generator.rb
+++ lib/rubber/generator.rb
@@ -28,19 +28,20 @@ module Rubber
         config_dirs << "#{@config_dir}/host/#{@host}"
 
         pat = Regexp.new(file_pattern) if file_pattern
-
+        files = []
         Find.find(*config_dirs) do |f|
           Find.prune if f =~ /\/(CVS|\.svn)\/|~$/
-          if File.file?(f) && (! pat || pat.match(f))
-            LOGGER.info{"Transforming #{f}"}
-            begin
-              transform(IO.read(f), @options)
-            rescue Exception => e
-              lines = e.backtrace.grep(/^\(erb\):([0-9]+)/) {|b| Regexp.last_match(1) }
-              LOGGER.error{"Transformation failed for #{f}#{':' + lines.first if lines.first}"}
-              LOGGER.error e.message
-              exit 1
-            end
+          files << f if File.file?(f) && (! pat || pat.match(f))
+        end
+        files.sort.each do |f|
+          LOGGER.info{"Transforming #{f}"}
+          begin
+            transform(IO.read(f), @options)
+          rescue Exception => e
+            lines = e.backtrace.grep(/^\(erb\):([0-9]+)/) {|b| Regexp.last_match(1) }
+            LOGGER.error{"Transformation failed for #{f}#{':' + lines.first if lines.first}"}
+            LOGGER.error e.message
+            exit 1
           end
         end
       end
